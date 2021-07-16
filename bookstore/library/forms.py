f82dc32fb3333c782b4e1166496d90283a3fa917
wishwashan <64398675+wishwashan@users.noreply.github.com>
2021-07-16 23:28:56 +0530

    Add files via upload

---
+++ bookstore/library/forms.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from bootstrap_modal_forms.mixins import PopRequestMixin, CreateUpdateAjaxMixin
+from django.forms import ModelForm
+from library.models import Book
+from django import forms
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ('title', 'author_firstName', 'author_lastName', 'genre', 'height', 'publisher')        
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')
